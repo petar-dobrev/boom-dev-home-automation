acf67e7423a164c39f612231e50b253a650965ea
Petar Dobrev <petar.dobrev92@gmail.com>
2023-02-20 15:09:31 +0200

    fix: css styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,11 @@ import Paper from "@mui/material/Paper";
 
 export default function Header({ right, left }) {
   return (
-    <Paper className={classNames(styles["header-wrapper"])}>
-      <div className={classNames(styles.left)}>{left}</div>
-      <div className={classNames(styles.right)}>{right}</div>
-    </Paper>
+    <div className={classNames(styles["header-wrapper"])}>
+      <Paper className={classNames(styles.header)}>
+        <div className={classNames(styles.left)}>{left}</div>
+        <div className={classNames(styles.right)}>{right}</div>
+      </Paper>
+    </div>
   );
 }
