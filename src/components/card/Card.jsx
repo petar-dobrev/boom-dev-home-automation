e0e2e99c14cd0a04649391517a457a1015a965cd
Petar Dobrev <petar.dobrev92@gmail.com>
2023-02-21 15:04:24 +0200

    feat: add Card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+
+import { CardContent } from "@mui/material";
+import { Card as MUICard } from "@mui/material";
+
+import styles from "./Card.module.scss";
+
+export default function Card({ iconUrl, outlined = "false" }) {
+  return (
+    <div className={classNames(styles.card)}>
+      <MUICard
+        className={outlined && styles.outlined}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "transparent",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <CardContent>
+          <img src={iconUrl} alt="icon" />
+        </CardContent>
+      </MUICard>
+    </div>
+  );
+}
