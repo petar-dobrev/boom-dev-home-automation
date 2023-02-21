ee4aac80b88d8ee6c16d631fcb9413b98fb4ebdc
Petar Dobrev <petar.dobrev92@gmail.com>
2023-02-21 15:05:08 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,57 +1,65 @@
 import { Button, InputAdornment } from "@mui/material";
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
 import classNames from "classnames";
 import React from "react";
-import TextField from '@material-ui/core/TextField';
-import styles from './Login.module.scss'
-import ReactDOM  from "react-dom";
+import TextField from "@mui/material";
+import styles from "./Login.module.scss";
+import ReactDOM from "react-dom";
 
-export default function Login()  {
-
-    return (
-        <div className={classNames(styles["login-form"])}>
-            <form className={classNames(styles.login)} noValidate autoComplete="on">
-                <TextField className={classNames(styles.text)}
-                    id="input-email"
-                    type="text"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <EmailIcon className={classNames(styles.icon)}
-                                    style={{
-                                        color: "black"
-                                    }} />
-                            </InputAdornment>
-                        ),
-                        disableUnderline: true,
-                    }}
+export default function Login() {
+  return (
+    <div className={classNames(styles["login-form"])}>
+      <form className={classNames(styles.login)} noValidate autoComplete="on">
+        <TextField
+          className={classNames(styles.text)}
+          id="input-email"
+          type="text"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EmailIcon
+                  className={classNames(styles.icon)}
+                  style={{
+                    color: "black",
+                  }}
                 />
-                <TextField className={classNames(styles.password)}
-                    id="input-email"
-                    type="password"
-                    style={{
-                        color: "black",
-                        boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.3)"
-                    }}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <LockIcon style={{
-                                    color: "black"
-                                }}
-                                    className={classNames(styles.icon)} />
-                            </InputAdornment>
-                        ),
-                        disableUnderline: true,
-                    }}
+              </InputAdornment>
+            ),
+            disableUnderline: true,
+          }}
+        />
+        <TextField
+          className={classNames(styles.password)}
+          id="input-email"
+          type="password"
+          style={{
+            color: "black",
+            boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.3)",
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon
+                  style={{
+                    color: "black",
+                  }}
+                  className={classNames(styles.icon)}
                 />
+              </InputAdornment>
+            ),
+            disableUnderline: true,
+          }}
+        />
 
-                <Button className={classNames(styles.button)} type="login" variant="contained">Login</Button>
-
-
-            </form>
-        </div>
-    );
+        <Button
+          className={classNames(styles.button)}
+          type="login"
+          variant="contained"
+        >
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 }
-
